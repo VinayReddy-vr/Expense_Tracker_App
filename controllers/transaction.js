d24565553546d86867697f73c769083733125344
Vinay Kumar Reddy Vallapu Reddy <137642809+VinayReddy-vr@users.noreply.github.com>
2023-10-02 17:07:08 -0400

    Add files via upload

---
+++ controllers/transaction.js
@@ -0,0 +1,87 @@
+//bring the model
+const Transaction = require('../models/Transaction')
+//@desc Get all transactions
+//@route GET /api/v1/transactions
+//access : public
+exports.getTransactions = async (req,res,next) =>{
+    try{
+        const transactions = await Transaction.find();
+        return res.status(200).json({
+            success: true,
+            count:transactions.length,
+            data:transactions
+        });
+    }catch(err)
+ {
+    return res.status(500).json({
+        success:false,
+        error:'Server'
+    })
+ }
+}
+
+//@desc POST all transactions
+//@route POST /api/v1/transactions
+//access : public
+exports.addTransactions = async (req,res,next) =>{
+
+   try{
+    const {text,amount} = req.body;
+    const transaction = await Transaction.create(req.body);
+    return res.status(201).json({
+        success: true,
+        data: transaction
+    });
+   }
+   catch(err)
+   {
+    // console.log(err)
+    // if(err.name==='ValidationError')
+    // {
+    //     const messages = Object.values(err.errors).map(val=>val.message)
+    //     return res.status(400).json({
+    //         success:false,
+    //         error:messages
+    //     });
+        
+    // }else
+    // {
+    //     return res.status(500).json({
+    //         success:false,
+    //         error:'Server'
+    //     })
+    console.log(err)
+
+   } 
+    
+
+}
+
+//@desc  all transactions
+//@route DELETE /api/v1/transactions/:id
+//access : public
+exports.deleteTransactions = async (req,res,next) =>{
+    try{
+    const transaction = await Transaction.findById(req.params.id);
+    if(!transaction)
+    {
+       return res.status(404).json({
+           success:false,
+           error:"No transaction found "
+       })
+    }
+
+    await Transaction.deleteOne({ _id: req.params.id }); 
+
+    return res.status(200).json({
+        success:true,
+        data:{transaction}
+    })
+}catch(err)
+{
+    return res.status(500).json({
+        success:false,
+        error: err.message
+    })
+}
+}
